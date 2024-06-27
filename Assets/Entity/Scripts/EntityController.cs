191a2fda066c2a8510e534e66d4a1238337338c6
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-06-27 18:28:32 +0900

    アイテムの取得・与ダメージ判定をItemControllerに移動 一定以上の運動速度がないとアイテムがダメージを与えられないようにした

--- Assets/Entity/Scripts/EntityController.cs
+++ Assets/Entity/Scripts/EntityController.cs
@@ -18,43 +18,29 @@ public class EntityController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<ItemStatsContainer>() != null)
-        {
-            //プレイヤーの攻撃に当たった場合ダメージを受けて攻撃を破壊する
-            if (collision.gameObject.CompareTag(Tag.PlayerDropItem) || collision.gameObject.CompareTag(Tag.PlayerAttack))
-            {
-                var itemController = collision.GetComponent<ItemStatsContainer>();
-                TakeDamage(itemController);
-                if (collision.gameObject.CompareTag(Tag.PlayerDropItem))
-                {
-                    Destroy(collision.gameObject);
-                }
-            }
-
-            if (status.hp <= 0)
-            {
-                foreach (var drop in entityData.drops)
-                {
-                    for (int i = 0; i < drop.amount + Random.Range(0, drop.addFluctuation + 1); i++)
-                    {
-                        ItemObjectCreator.CreateAndDrop(gameObject, drop.dropItem, 2, 0, Tag.EntityDropItem);
-                    }
-                }
-                Destroy(gameObject);
-            }
-        }
-
         if (collision.CompareTag(Tag.Player))
         {
             PlayerController.TakeDamage(entityData.atk);
         }
     }
 
-    private void TakeDamage(ItemStatsContainer itemController)
+    public void TakeDamage(ItemStatsContainer itemController)
     {
         var damage = (int)Math.Clamp((itemController.atk + PlayerController.Status.bonusAtk - entityData.def), 0, 9999);
         status.hp -= damage;
         GetComponent<DamageNumberEffect>().CreateDamageNumberObject(damage);
+
+        if (status.hp <= 0)
+        {
+            foreach (var drop in entityData.drops)
+            {
+                for (int i = 0; i < drop.amount + Random.Range(0, drop.addFluctuation + 1); i++)
+                {
+                    ItemObjectCreator.CreateAndDrop(gameObject, drop.dropItem, 2, 0, Tag.EntityDropItem);
+                }
+            }
+            Destroy(gameObject);
+        }
     }
 }
 
--- Assets/Items/Objects/Scripts/ItemController.cs
+++ Assets/Items/Objects/Scripts/ItemController.cs
@@ -2,12 +2,43 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //プレイヤーアイテム取得処理
         if (CompareTag(Tag.EntityDropItem) && collision.CompareTag(Tag.Player))
         {
             Inventory.AddItem(GetComponent<ItemStatsContainer>().ID);
             Destroy(gameObject);
+        }
+
+        //敵ヒット処理
+        if (collision.CompareTag(Tag.Entity))
+        {
+            if (rb != null)
+            {
+                //一定以上の運動速度を持ってないと判定しない（移動の速さが1以上、または回転の速さが100以上であれば判定）
+                if (rb.velocity.magnitude < 1 && Mathf.Abs(rb.angularVelocity) < 100)
+                {
+                    return;
+                }
+            }
+            var entity = collision.GetComponent<EntityController>();
+            //プレイヤーの攻撃に当たった場合ダメージを受けて攻撃を破壊する
+            if (CompareTag(Tag.PlayerDropItem) || CompareTag(Tag.PlayerAttack))
+            {
+                var itemController = GetComponent<ItemStatsContainer>();
+                entity.TakeDamage(itemController);
+
+                if (CompareTag(Tag.PlayerDropItem))
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
