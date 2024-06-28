bd8018f0ba62d88cd03fd5cfa5b609820680b8f3
IwakiEisuke <get10.ref11.del12@gmail.com>
2024-06-29 02:39:28 +0900

    ゲームオーバーSEとゲームクリアBGMを追加・全体の音量バランスの調整

--- Assets/Effects/Damage/DamageNumberEffect.cs
+++ Assets/Effects/Damage/DamageNumberEffect.cs
@@ -1,10 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class DamageNumberEffect : MonoBehaviour
 {
+    [SerializeField] AudioClip damageSE, healSE;
     GameObject canvas, numberObj;
     Vector2 pos;
     private void Start()
@@ -21,10 +21,11 @@ public class DamageNumberEffect : MonoBehaviour
         GetComponent<SpriteRenderer>().color = Color.red;
         StartCoroutine(ResetColor());
         numberObj = Instantiate(EffectSettings._damageNumberObject, canvas.transform);
+        numberObj.GetComponent<AudioSource>().PlayOneShot(damageSE);
         numberObj.GetComponent<DamageNumberEffectController>().pos = transform.position;
         var tmp = numberObj.GetComponent<TextMeshProUGUI>();
         tmp.text = amount.ToString();
-        if(amount >= PlayerController.Status.maxHP / 2)
+        if (amount >= PlayerController.Status.maxHP / 2)
         {
             tmp.fontSize = 54;
         }
@@ -36,6 +37,7 @@ public class DamageNumberEffect : MonoBehaviour
         //GetComponent<SpriteRenderer>().color = Color.green;
         StartCoroutine(ResetColor());
         numberObj = Instantiate(EffectSettings._damageNumberObject, canvas.transform);
+        numberObj.GetComponent<AudioSource>().PlayOneShot(healSE);
         numberObj.GetComponent<DamageNumberEffectController>().pos = transform.position;
         var tmp = numberObj.GetComponent<TextMeshProUGUI>();
         tmp.text = amount.ToString();
--- Assets/Items/Skills/Scripts/Throw.cs
+++ Assets/Items/Skills/Scripts/Throw.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 public class Throw : ItemActionBase
 {
     [SerializeField] AudioClip throwSE;
+    [SerializeField] float SEVolume;
     public override void Action(ItemUseController controller, InventoryItemData item)
     {
         item.amount--;
         var throwItem = ItemObjectCreator.CreateAndThrow(controller.player, item, Tag.PlayerDropItem);
-        var audio = throwItem.AddComponent<AudioSource>();
-        audio.clip = throwSE;
-        audio.Play();
-        Destroy(audio, throwSE.length);
+        if (throwSE != null)
+        {
+            var audio = throwItem.AddComponent<AudioSource>();
+            audio.clip = throwSE;
+            audio.volume = SEVolume;
+            audio.Play();
+            Destroy(audio, throwSE.length);
+        }
     }
 }
